0fdd98bec5dd48bb44bbddf63bd9a6f254dca9ed
Caio Pumar <caiopumar@gmail.com>
2018-08-14 15:51:31 -0300

    daily_update skeleton added

--- slowcontrol_pannel/slowdata.py
+++ slowcontrol_pannel/slowdata.py
@@ -1,14 +1,27 @@
 import os
 import sys
+import configparser
+
 import matplotlib
 matplotlib.use('Agg') # Don't try to use X forwarding for plots
 import matplotlib.pyplot as plt
+
 import pandas as pd
 import numpy as np
 from root_pandas import read_root
 
 _,mostRecentDir = sys.argv
-plotoutDir = '/home/caio/Documents/Plots'
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+plotoutDir = config['SlowControl']['plotoutdir']
+
+pressrange = getconfigrange('AlarmRanges', 'pressure')
+temprange = getconfigrange('AlarmRanges', 'temperature')
+humidrange = getconfigrange('AlarmRanges', 'humidity')
+magrange = getconfigrange('AlarmRanges', 'magfield')
+
 
 for file in os.listdir(mostRecentDir):
 	if file.endswith('.sroot'):
@@ -27,9 +40,9 @@ fileNameStems = ['temperature', 'pressure', 'humidity', 'magfield']
 
 #Outlier filtering (when arduino and labview are out of sync, wild values for slow control are recorded)
 slowdata = slowdata[(slowdata.temp > 25) & (slowdata.temp < 35)]
-slowdata = slowdata[(slowdata.pres > 101000) & (slowdata.pres < 102000)]
-slowdata = slowdata[(slowdata.btot > 0) & (slowdata.btot < 1000)]
-slowdata = slowdata[(slowdata.humid > 40) & (slowdata.humid < 50)]
+slowdata = slowdata[(slowdata.pres > 100000) & (slowdata.pres < 103000)]
+slowdata = slowdata[(slowdata.btot > 0) & (slowdata.btot < 1500)]
+slowdata = slowdata[(slowdata.humid > 0) & (slowdata.humid < 100)]
 for hv in hvs:
     slowdata = slowdata[(slowdata[hv] > slowdata[hv].mean() - 3) & (slowdata[hv] < slowdata[hv].mean() + 3)]
 
