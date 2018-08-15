1e6859ee0dae0c918411904799f19961b8d788d5
Caio Pumar <caiopumar@gmail.com>
2018-08-15 15:56:47 -0300

    graph stylization improved

--- slowcontrol_pannel/slowdata.py
+++ slowcontrol_pannel/slowdata.py
@@ -26,13 +26,13 @@ temprangealarm = getconfigrange('AlarmRanges', 'temperature')
 humidrangealarm = getconfigrange('AlarmRanges', 'humidity')
 magrangealarm = getconfigrange('AlarmRanges', 'magfield')
 
-"""
-not needed
-pressrange = getconfigrange('PlotRanges', 'pressure')
-temprange = getconfigrange('PlotRanges','temperature')
-humidrange = getconfigrange('PlotRanges','humidity')
-magrange = getconfigrange('PlotRanges','magfield')
-"""
+dataIndices = ['temp', 'pres', 'humid', 'btot']
+hvs = ['hv'+str(i) for i in range(0,8)]
+axisLabels = ["Temperature [Deg]", "Pressure [Pa]", "Humidity [%]", "Magnetic field [Xgauss]"]
+plotTitles = ["Temperature", "Pressure", "Humidity", "Magnetic field"]
+fileNameStems = ['temperature', 'pressure', 'humidity', 'magfield']
+
+###Get slow data
 for file in os.listdir(mostRecentDir):
 	if file.endswith('.sroot'):
 		sroot = file
@@ -42,38 +42,31 @@ slowdata['stime'] = slowdata['stime'] - 2208988800 # Convert from Mac to UNIX ti
 slowdata['stime'] = pd.to_datetime(slowdata['stime'], unit = 's')
 slowdata = slowdata.rename(columns = {'stime' : 'time'})
 
-dataIndices = ['temp', 'pres', 'humid', 'btot']
-hvs = ['hv'+str(i) for i in range(0,8)]
-axisLabels = ["Temperature [Deg]", "Pressure [Pa]", "Humidity [%]", "Magnetic field [Xgauss]"]
-plotTitles = ["Temperature", "Pressure", "Humidity", "Magnetic field"]
-fileNameStems = ['temperature', 'pressure', 'humidity', 'magfield']
 
-
-"""
-for vl in slowdata.columns:
-	print vl+',  std:  '+str(slowdata[vl].std())
-"""
 
 #Outlier filtering (when arduino and labview are out of sync, wild values for slow control are recorded)
 slowdata = slowdata[(slowdata.temp > 25) & (slowdata.temp < 35)]
 slowdata = slowdata[(slowdata.pres > 100000) & (slowdata.pres < 103000)]
 slowdata = slowdata[(slowdata.btot > 0) & (slowdata.btot < 1500)]
 slowdata = slowdata[(slowdata.humid > 42) & (slowdata.humid < 50)]
-"""
-for hv in hvs:
-    slowdata = slowdata[(slowdata[hv] > slowdata[hv].mean() - 5) & (slowdata[hv] < slowdata[hv].mean() + 5)]
-"""
 
-"""
 for hv in hvs:
-    plot= slowdata.plot(x='time',y=hv)
+    slowdata = slowdata[(slowdata[hv] > slowdata[hv].median() - 5) & (slowdata[hv] < slowdata[hv].median() + 5)]
+#
+
+
+for hv in hvs:
+    plot= slowdata.plot(x='time',y=hv,title='High Voltage')
+    ax = plt.gca()
+    plt.ylim(slowdata[hv].median() - 1.5, slowdata[hv].median() + 1.5)
     fig = plot.get_figure()
-    fig.savefig(plotoutDir+'/'+hv+"_output.png")
-"""
+    fig.savefig(plotoutDir+'/high_voltage'+hv[-1]+".png")
 
-for di,fn in zip(dataIndices,fileNameStems):
-	plot = slowdata.plot(x='time',y=di)
+for i in range(4):
+	plot = slowdata.plot(x='time',y=dataIndices[i],title=plotTitles[i],figsize=(14,10))
 	ax = plt.gca()
-	plt.ylim(getconfigrange('PlotRanges',fn))
+	plt.ylim(getconfigrange('PlotRanges',fileNameStems[i]))
+	plt.ylabel(axisLabels[i])
 	fig = plot.get_figure()
-	fig.savefig(plotoutDir+'/'+di+"_output.png")
+	fig.savefig(plotoutDir+'/'+fileNameStems[i]+".png")
+
