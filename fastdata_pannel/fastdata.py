9f4ffb5d09d4d2a35a0cee9e3b52eadedce2f0c7
Caio Pumar <caiopumar@gmail.com>
2018-09-17 17:12:58 -0300

    fast data indexing and plotting, first tests

--- fastdata_pannel/fastdata.py
+++ fastdata_pannel/fastdata.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 from root_pandas import read_root
 
-### Insert path to most recent directory with slow data
+### Insert path to most recent directory with fast data
 _,mostRecentDir = sys.argv
 
 rootf=[]
@@ -18,8 +18,17 @@ for file in os.listdir(mostRecentDir):
 	if file.endswith('.root'):
 		rootf.append(file)
 
+df = read_root('/home/caio/Documents/processing_data/mx_b_20180716_1742/mx_b_20180716_1742_000000.root', columns=['channel','integral','time','istestpulse','error','baseline','rms','ratio','height'])
+df = df[(df['channel']==3) & (df.error==0) & (df.integral > 3000)]
+#df['time'] = df['time'] - 2208988800
+#df['time'] = pd.to_datetime(df['time'], unit = 's')
+plot = df.plot.scatter(x='time',y='integral')
+ax = plt.gca()
+fig = plot.get_figure()
+fig.savefig('testfig.png')
+"""
 if len(rootf)>1:
-	framesroot = [read_root(mostRecentDir+'/'+rf) for rf in rootf]
+	framesroot = [read_root(mostRecentDir+'/'+rf, columns=[]) for rf in rootf]
 	fastdata = pd.concat(framesroot,axis=1)
 
 else:
@@ -28,4 +37,5 @@ fastdata['time'] = fastdata['time'] - 2208988800 # Convert from Mac to UNIX time
 fastdata['time'] = pd.to_datetime(fastdata['time'], unit = 's')
 
 
-print(fastdata[['integral','height','time']].head())
+print(fastdata.columns)
+"""
