9920016b13465a350e6e159846ef3ac61ac0d243
Caio Pumar <caiopumar@gmail.com>
2018-09-28 17:16:40 -0300

    partial spectrum plotting fixed

--- fastdata_pannel/fastdata.py
+++ fastdata_pannel/fastdata.py
@@ -34,23 +34,38 @@ else:
 
 df = df[df.error==0]
 
-df_spec = df.groupby(pd.cut(df['integral'],bins=500)).agg('count').rename(columns={'integral' : 'emission count'}).reset_index()
+df=df[df.channel==3]
+df_spec = df.groupby(pd.cut(df['integral'],bins=500)).agg('count').rename(columns={'integral' : 'emission_count'}).reset_index()
 df_spec = df_spec.rename(columns={'integral' : 'energy'})
 df_spec['energy'] = df_spec['energy'].astype('str')
-df_spec['energy'] = df_spec['energy'].str.split(',').str[0].str.split('.').str[0].str[1:]
-
-print (df_spec['energy'].head(10))
-
+df_spec['energy'] = df_spec['energy'].str.split(',').str[0].str.split('.').str[0].str[1:].astype('int')
+df_spec = df_spec[['energy','emission_count']]
+plot2 = df_spec.plot(x='energy',y='emission_count')
+ax2 = plt.gca()
+fig2 = plot2.get_figure()
+fig2.savefig(plotoutDir+'/' + 'spectrum_channel.png')
 """
 for i in range (0,8):
-    df_channel = df[(df['channel']==i)]
-    df_channel = df_channel[['time','integral']]
+    df_channel = df[(df['channel']==i)][['time','integral']]
+    #Putting together the spectrum
+    df_spec = df_channel.groupby(pd.cut(df['integral'],bins=500)).agg('count').rename(columns={'integral' : 'emission_count'}).reset_index()
+	df_spec = df_spec.rename(columns={'integral' : 'energy'})
+	df_spec['energy'] = df_spec['energy'].astype('str')
+	df_spec['energy'] = df_spec['energy'].str.split(',').str[0].str.split('.').str[0].str[1:].astype('int')
+	df_spec = df_spec[['energy','emission_count']]
+	#Emission rate
     df_channel['time'] = df_channel['time'] - 2208988800
     df_channel['time'] = pd.to_datetime(df_channel['time'], unit = 's')
     df_channel = df_channel.set_index('time').resample('10T').count().dropna().reset_index()
     df_channel = df_channel.iloc[1:-1]
-    plot = df_channel.plot(x='time',y='integral')
-    ax = plt.gca()
-    fig = plot.get_figure()
-    fig.savefig(plotoutDir+ '/' + 'emission_count_channel'+str(i)+'.png')
+    #Plotting for both regimes and all 8 channels
+    plot1 = df_channel.plot(x='time',y='integral')
+    ax1 = plt.gca()
+    fig1 = plot1.get_figure()
+    fig1.savefig(plotoutDir+ '/' + 'emission_count_channel'+str(i)+'.png')
+    plot2 = df_spec.plot(x='energy',y='emission_count')
+    ax2 = plt.gca()
+    ax1.ylabel('emission count')
+    fig2 = plot2.get_figure()
+    fig2.savefig(plotoutdir+'/' + 'spectrum_channel'+str(i)+'.png')
 """
