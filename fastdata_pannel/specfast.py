ddce1c5e9534313e875df9cfaf6e8edc50f155d6
Caio Pumar <caiopumar@gmail.com>
2018-10-22 17:47:36 -0200

    added monte carlo simulations for plot comparison

--- fastdata_pannel/specfast.py
+++ fastdata_pannel/specfast.py
@@ -26,17 +26,44 @@ for file in os.listdir(mostRecentDir):
 
 if len(rootf)>1:
 	framesroot = [read_root(mostRecentDir+'/'+rf, columns=['channel','time','error','integral']) for rf in rootf]
-	df = pd.concat(framesroot,axis=0)
+	dataframe = pd.concat(framesroot,axis=0)
 else:
-	df = read_root(mostRecentDir+'/'+rootf[0], columns=['channel','time','error','integral'])
+	dataframe = read_root(mostRecentDir+'/'+rootf[0], columns=['channel','time','error','integral'])
 
-def perc_error(dframe):
-    return 
 
-df = df[(df.integral>0)]
+dataframe = dataframe[dataframe.integral>0]
 
 channel_label = ['Cs-137','Cs-137','Co-60','Co-60','Background','Background','Ti-44','Ti-44']
+def timedelta(df):
+    df['time'] = df['time'] - 2208988800
+    df['time'] = pd.to_datetime(df['time'], unit = 's')
+    return (df['time'].min()-df['time'].max()).seconds
 
+
+def specdf(df,channel):
+    df = df[(df['channel']==channel)][['time','integral']]
+    td = timedelta(df)
+    df = df.groupby(pd.cut(df['integral'],bins=500)).agg('count').rename(columns={'integral' : '#emission'}).reset_index()
+    df = df.rename(columns={'integral' : 'energy'})
+    df['energy'] = df['energy'].astype('str')
+    df['energy'] = df['energy'].str.split(',').str[0].str.split('.').str[0].str[1:].astype('int')
+    df = df[['energy','#emission']]
+    df['#emission'] = df['#emission']/(td)
+    return df
+
+def plot_spectra(df,channel):
+    df_spec = specdf(df[df.error==0],channel)
+    df_spec_e = specdf(df[df.error!=0],channel)
+    plt.figure()
+    ax = df_spec.plot(x='energy',y='#emission',logy=True)
+    df_spec_e.plot(x='energy',y='#emission',logy=True, ax = ax)
+    ax.legend(['Error free emissions','With error'])
+    plt.ylabel('Rate (Hz) / ' +str( ((df_spec['energy'].max()-df_spec['energy'].min())/500) )+' KeV' )
+    plt.savefig(plotoutDir+'/test_spectra_cs.png')
+
+plot_spectra(dataframe,0)
+
+"""
 for label,i in zip(channel_label,range (0,1)):
     df_channel = df[(df['channel']==i)][['time','integral']]
     df_spec = df_channel.groupby(pd.cut(df_channel['integral'],bins=500)).agg('count').rename(columns={'integral' : '#emission'}).reset_index()
@@ -44,10 +71,14 @@ for label,i in zip(channel_label,range (0,1)):
     df_spec['energy'] = df_spec['energy'].astype('str')
     df_spec['energy'] = df_spec['energy'].str.split(',').str[0].str.split('.').str[0].str[1:].astype('int')
     df_spec = df_spec[['energy','#emission']]
-    plot2 = df_spec[df_spec['error'==0]].plot(x='energy',y='#emission', title = label+' - channel '+str(i),logy=True)
     for er in df_spec['error'].unique():
-        df_spec[df_spec['error']==er].plot(x='energy',y='#emission',logy='True')
+        if er==0:
+            dfe = df_spec[df_spec['error']==0]
+            plot2 = dfe.plot(x='energy',y='#emission', title = label+' - channel '+str(i),logy=True)
+        else:
+            dfe = df_spec[df_spec['error']==er]
+            dfe.plot(x='energy',y='#emission',logy='True')
     plt.ylabel('Emission Count')
     fig2 = plot2.get_figure()
     fig2.savefig(plotoutDir+'/spectrum_'+label+'_channel'+str(i)+'.png')
-
+"""
