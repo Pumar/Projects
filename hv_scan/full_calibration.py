332a4a97a3982dbd732d16259046242a398e90a5
Caio Pumar <caiopumar@gmail.com>
2019-01-22 16:17:45 -0200

    beautified plots

--- hv_scan/full_calibration.py
+++ hv_scan/full_calibration.py
@@ -27,6 +27,8 @@ hv_file_list = ['/home/caio/Documents/processing_data/HV_Scan_2018_12/Processed/
                 '/home/caio/Documents/processing_data/HV_Scan_2018_12/Processed/mx_b_20181211_1628',
                 '/home/caio/Documents/processing_data/HV_Scan_2018_12/Processed/mx_b_20181211_1644']
 
+sig = r'$\sigma$'
+mu = r'$\mu$'
 """
 ### Care to change to corrrect hv0
 hv_file_list = ['/home/caio/Documents/processing_data/HV_Scan_2018_12/Processed/mx_b_20190115_1737']
@@ -75,11 +77,20 @@ def exp_fit(x, *p):
     return a*np.exp(-b*x)
 
 def peak_width(df,channel):
+    ###########################################################################
+    #
+    ### Input:
+    # df - dataframe with data to be fit (pd.DataFrame)
+    # channel - channel number corresponding to the detector (int)
+    ##
+    ### Obs:
+    ### f = A/B, used approx from https://www.sagepub.com/sites/default/files/upm-binaries/6427_Chapter_4__Lee_(Analyzing)_I_PDF_6.pdf
+    ###check wiki page for error propagation
+    #
+    ###########################################################################
     df_spec = specdf(df[df.error==0],channel)
-    #Emin = float(df_spec[df_spec['energy']==df_spec['energy'].min()]['energy'])
-    #Emax = float(df_spec[df_spec['Count']==df_spec['Count'].max()]['energy'])
     if channel == 0 or channel == 1:
-        df_spec = df_spec[df_spec['energy']>500]
+        df_spec = df_spec[df_spec['energy']>470]
         df_spec = df_spec[df_spec['energy']<(Cs_peaks+200)]
         ### p0 has the estimates for fitting coefficients (Height of peak{A}, Mean{mu}, Standard Deviation{sigma})
         p0 = [df_spec['Count'].max().astype('float'), Cs_peaks, 20., 2000, 0.001]
@@ -87,47 +98,54 @@ def peak_width(df,channel):
         coeff, var_matrix = curve_fit(exp_gauss, df_spec['energy'], df_spec['Count'], p0=p0, bounds=bounds, maxfev = 5000)
         perr = np.sqrt(np.diag(var_matrix))
         dm = coeff[2]/coeff[1]
-        return coeff, dm, np.absolute(dm)*np.sqrt(np.power(perr[2]/coeff[2],2)+np.power(perr[1]/coeff[1],2))
+        return coeff, perr, dm, np.absolute(dm)*np.sqrt(np.power(perr[2]/coeff[2],2)+np.power(perr[1]/coeff[1],2))
     else:
         df_spec = df_spec[df_spec['energy']>(1000)]
         df_spec = df_spec[df_spec['energy']<(Co_peaks[1]+350)]  
-        ### p0 has the estimates for fitting coefficients (Height of peak{A}, Mean{mu}, Standard Deviation{sigma})x2
+        ### p0 has the estimates for fitting coefficients (Height of peak{A}, Mean{mu}, Standard Deviation{sigma})x2 + (exp C1,C2) 
         p0 = [df_spec['Count'].max().astype('float'), Co_peaks[0], 20., df_spec['Count'].max().astype('float')*0.8, Co_peaks[1], 20., 1000, 0.00001]
         bounds = ([-np.inf,-np.inf,-np.inf,-np.inf,-np.inf,-np.inf,-np.inf,0],[np.inf,np.inf,np.inf,np.inf,np.inf,np.inf,np.inf,np.inf])
         coeff, var_matrix = curve_fit(exp_double_gauss, df_spec['energy'], df_spec['Count'], p0=p0, maxfev = 5000)
         perr = np.sqrt(np.diag(var_matrix))
-        print(coeff[-1])
         dm = coeff[2]/coeff[1]
         ### f = A/B, used approx from https://www.sagepub.com/sites/default/files/upm-binaries/6427_Chapter_4__Lee_(Analyzing)_I_PDF_6.pdf
         #check wiki page for error propagation
-        return coeff, dm, np.absolute(dm)*np.sqrt(np.power(perr[2]/coeff[2],2)+np.power(perr[1]/coeff[1],2))
+        return coeff, perr, dm, np.absolute(dm)*np.sqrt(np.power(perr[2]/coeff[2],2)+np.power(perr[1]/coeff[1],2))
 
 def plot_calib(hv,de,err,channel):
     title = 'deltaE_X_highvoltage_channel'+str(channel)+'.png'
     d = {'hv' : hv, 'de' : de, 'err' : err}
     df = pd.DataFrame(data=d)
     plt.figure()
-    df.plot(x='hv',y='de',yerr = 'err', kind='scatter',title = ' Unitless Energy Peak Width X High Voltage ')
+    df.plot(x='hv',y='de',yerr = 'err', kind='scatter',title = 'Normalized Energy Peak Width (standard deviation/mean) X High Voltage ')
+    plt.xlabel('Voltage (V)')
+    plt.ylabel(r'$\sigma$'+'/'+'$\mu$')
     plt.savefig(plotoutDir + '/hv_scan/'+title)
     plt.close()
 
-def plot_spectra(df,channel,hv,pf):
+def plot_spectra(df,channel,hv,pf, pf_err):
     title = 'hv'+str(hv)
     df_spec = specdf(df[df.error==0],channel)
     Emax = int((df_spec[df_spec['Count']==df_spec['Count'].max()]['energy']).astype('int'))
     Cmax = df_spec['Count'].max()
     if channel == 0 or channel == 1:
-        df_spec = df_spec[df_spec['energy']>500]
-        df_spec = df_spec[df_spec['energy']<(Cs_peaks+200)]
-        fit = exp_gauss(df_spec['energy'],*pf)
+        e_range = pd.Series([i for i in range(470,int(Cs_peaks)+200)])
+        fit = exp_gauss(e_range,*pf)
+        df_spec = df_spec[df_spec['energy']<Cs_peaks*2]
+        mult_val = 1.2
     else:
-        df_spec = df_spec[df_spec['energy']>(1000)]
-        df_spec = df_spec[df_spec['energy']<(Co_peaks[1]+350)]
-        fit = exp_double_gauss(df_spec['energy'],*pf)
+        e_range = pd.Series([i for i in range(1000,int(Co_peaks[1])+350)])
+        fit = exp_double_gauss(e_range,*pf)
+        df_spec = df_spec[df_spec['energy']<sum(Co_peaks)*1.1]
+        mult_val = 1.4
+    Emax = int((df_spec[df_spec['Count']==df_spec['Count'].max()]['energy']).astype('int'))
+    Cmax = df_spec['Count'].max()
     plt.figure()
-    df_spec.plot(x='energy',y='Count',yerr= 'y_err', kind='scatter',color='k', title = 'detector'+str(channel))
-    plt.plot(df_spec['energy'], fit,color='r')
-    plt.text(Emax*1.8, Cmax/2,'Epeak = '+str(Emax)+'keV')
+    df_spec.plot(x='energy',y='Count',yerr= 'y_err', kind='scatter',s=4,color='k', title = channel_label[channel] + ' - Detector '+str(channel)+' with Voltage = '+str(hv) + ' V')
+    plt.plot(e_range, fit,color='r')
+    plt.text(Emax*mult_val, Cmax/2,'Main Energy Peak'+'\n'+r'$\mu$ = '+str(int(pf[1]))+r' $\pm$'+str(pf_err[1])[:3]+' keV'+ '\n' + r'$\sigma$ = '+str(coeff[2])[:5]+r' $\pm$'+str(pf_err[2])[:4] +' keV')
+    plt.xlabel('Energy (keV)')
+    plt.ylabel('Event Count')
     plt.savefig(plotoutDir + '/hv_scan/spectra/'+'detector'+str(channel)+'_'+title+'.png')
     plt.close()
 
@@ -144,11 +162,11 @@ for chn in range(0,4):
         ## Create dataframe for every data acquisition and overwrite previous one
         dataframe = append_dfs(mod_dir)
         hv_l.append(hv0[chn]-hv_file_list.index(mod_dir)*hv_step)
-        coeff,de_mu,de_mu_err = peak_width(dataframe,chn)
+        coeff, coeff_err, de_mu,de_mu_err = peak_width(dataframe,chn)
         err_l.append(de_mu_err)
         de_l.append(de_mu)
         hv_s = hv0[chn]-hv_file_list.index(mod_dir)*25
-        plot_spectra(dataframe,chn,hv_s,coeff)
+        plot_spectra(dataframe,chn,hv_s,coeff, coeff_err)
     plot_calib(hv_l,de_l,err_l,chn)
     print('channel'+str(chn)+' plotted')
         
