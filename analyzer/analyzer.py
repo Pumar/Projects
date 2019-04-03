e6fd3ff9a1af731ff78b02d693a4610dedf5ed96
pumar <caiopumar@gmail.com>
2019-04-03 18:26:55 -0300

    v2 analyzer.py

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -40,18 +40,12 @@ def timedelta(df):
     return (df['time'].min()-df['time'].max()).seconds
 
 def gauss(x, *p):
-    ##########################################################
-    ### Gaussian function
-    #
-    ## Check https://en.wikipedia.org/wiki/Gaussian_function
-    #
-    ### Input:
-    #
-    ## A, mu, sigma - fit params
-    #
-    ##########################################################
     A, mu, sigma = p
     return A*np.exp(-(x-mu)**2/(2.*sigma**2))
+
+def triple_gauss(x, *p):
+    A1, mu1, sigma1 = p
+    return A1*np.exp(-(x-mu1)**2/(2.*sigma1**2)) + A2*np.exp(-(x-mu2)**2/(2.*sigma2**2)) + A3*np.exp(-(x-mu3)**2/(2.*sigma3**2))
 
 def specdf(df,channel):
     df = df[(df['channel']==channel)][['time','integral']]
@@ -95,19 +89,32 @@ def compton_simulation(filename):
 def subtract_compton(df,channel):
     sim_count, sim_energy = compton_simulation(simulation_files[channel//2])
     fast_count,fast_energy = specdf(df,channel)
-    print(len(fast_energy),len(fast_count))
     bg_count, bg_energy = specdf(df,0)
+    ### Range fixing for simulation data
     sim_energy = np.array(filter(lambda x: x >= min(fast_energy),sim_energy))
-    sim_count = sim_count[len(sim_count)-len(sim_energy):]
+    if len(sim_count) != len(sim_energy):
+        sim_count = sim_count[len(sim_count)-len(sim_energy):]
     sim_energy = np.array(filter(lambda x: x <= max(fast_energy),sim_energy))
-    sim_count = sim_count[:(len(sim_energy)-len(sim_count))]
-    print(len(bg_energy),len(bg_count))
-    bg_energy = np.array(filter(lambda x: x >= min(fast_energy),sim_energy))
-    bg_count = sim_count[len(bg_count)-len(bg_energy):]
-    bg_energy = np.array(filter(lambda x: x <= max(fast_energy),sim_energy))
-    bg_count = sim_count[:(len(bg_energy)-len(bg_count))]  
-    print(len(bg_energy),len(bg_count))
-    sys.exit()
+    if len(sim_count) != len(sim_energy):
+        sim_count = sim_count[:(len(sim_energy)-len(sim_count))]
+    ##
+    ### Range fixing for background data
+    if len(bg_energy) >= len(fast_energy):
+        bg_energy = np.array(filter(lambda x: x >= min(fast_energy),bg_energy))
+        if len(bg_count) != len(bg_energy):
+            bg_count = bg_count[len(bg_count)-len(bg_energy):]
+        bg_energy = np.array(filter(lambda x: x <= max(fast_energy),bg_energy))
+        if len(bg_count) != len(bg_energy):
+            bg_count = bg_count[:(len(bg_energy)-len(bg_count))]
+    else:
+        print(min(bg_energy),max(bg_energy),min(fast_energy),max(fast_energy))
+        if bg_energy[-1] != fast_energy[-1]:
+            bg_count = np.append(bg_count,np.multiply([i for i in range(max(bg_energy),max(fast_energy),5)],0))
+            bg_energy = np.append(bg_energy,[i for i in range(max(bg_energy),max(fast_energy),5)])
+        if bg_energy[0] != fast_energy[0]:    
+            bg_count = np.append(np.multiply([i for i in range(min(fast_energy),min(bg_energy),5)],0),bg_count)
+            bg_energy = np.append([i for i in range(min(fast_energy),min(bg_energy),5)],bg_energy)
+    ##
     if channel == 2 or channel == 3:
         ratio = max(fast_count[int(np.where(fast_energy ==570)[0]):int(np.where(fast_energy == 800)[0])])/max(sim_count[int(np.where(sim_energy == 570)[0]):int(np.where(sim_energy == 800)[0])])
     elif channel == 4 or channel == 5:
@@ -115,13 +122,18 @@ def subtract_compton(df,channel):
     else:
         ratio = max(fast_count[int(np.where(fast_energy ==850)[0]):int(np.where(fast_energy == 1000)[0])])/max(sim_count[int(np.where(sim_energy == 850)[0]):int(np.where(sim_energy == 1000)[0])])
     sim_count = sim_count*ratio
+    
+    print(len(bg_count))
+    print(len(fast_count))
+
+    fast_count = np.subtract(fast_count,bg_count)
     return np.subtract(fast_count,sim_count),fast_energy,sim_count,fast_count
 
 
 dataframe = append_dfs()
 dataframe = dataframe[(dataframe['integral'] >= 0) & (dataframe['integral'] <= 3000) & (dataframe['error'] == 0)][['time','integral','channel']]
 
-for chn in range(4,5): 
+for chn in range(2,3): 
     final,energy,simulation_count,count = subtract_compton(dataframe,chn)
 
 
@@ -130,7 +142,7 @@ plt.scatter(energy,simulation_count,c='k',s=1)
 plt.scatter(energy,count,c='r',s=1)
 plt.scatter(energy,final,c='green',s=1)
 plt.axhline(y=0,c='k')
-plt.xlim(200,800)
+plt.xlim(300,1500)
 plt.savefig('compton.png')
 plt.close()
 
